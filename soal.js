cc34811b085f414e7a3b2d2e0779c4bb9af71e2d
Bean791 <bean90917@gmail.com>
2022-02-05 19:41:39 +0700

    revisi 2

--- soal.js
+++ soal.js
@@ -2,6 +2,7 @@
 var p =prompt('mau ulang berapa kali')
 var b=prompt('batas angka')
 var i= 0
+var n= 0
 while (i < p){
     var angka1=Math.floor(Math.random() * b ) + 1  
     var angka2=Math.floor(Math.random() * b ) + 1  
@@ -9,8 +10,14 @@ while (i < p){
     var ha= angka1+angka2
     if (ja==ha){
         alert("Jawaban betul")
+        n++;
     }else{
         alert("salah")
     }
-    i++;  
-}   
+    i++;
+
+}
+// persen = (laba / modal)*(100);
+// persen = persen.toFixed(2);
+nilai= n/i*(100)
+alert("Dari "+i+" soal benar adalah "+n+" Dan Nilainya adalah "+nilai) 
